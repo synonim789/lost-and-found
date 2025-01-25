4a720c44a5da9510554cd80a9ff68c0e5d470be9
synonim789 <75422461+synonim789@users.noreply.github.com>
2025-01-25 18:59:14 +0100

    setup frontend

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <section className="h-screen flex items-center justify-center">
+      <h1>
+        Lost and <span>Found</span>
+      </h1>
+    </section>
+  )
+}
+export default Login
