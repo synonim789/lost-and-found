0d35b02960a5f0e74ef785e82ebb04ec7f1b6897
synonim789 <75422461+synonim789@users.noreply.github.com>
2025-07-21 16:21:43 +0200

    add message notification menu

--- frontend/src/components/MessagesMenu.tsx
+++ frontend/src/components/MessagesMenu.tsx
@@ -1,18 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { MdOutlineMessage } from 'react-icons/md'
 import { RiMessage2Fill } from 'react-icons/ri'
+import { useNotifications } from '../hooks/useNotifications'
 import Dropdown from './Dropdown'
 
-const MessagesMenu = () => {
+type Props = {
+  userId: number
+}
+
+const MessagesMenu = ({ userId }: Props) => {
   const [open, setOpen] = useState<boolean>(false)
+  const menuRef = useRef<HTMLDivElement>(null)
+  const { notifications } = useNotifications(userId)
+
+  useEffect(() => {
+    const closeDropdown = (e: Event) => {
+      if (menuRef && !menuRef.current?.contains(e.target as Node)) {
+        setOpen(false)
+      }
+    }
+
+    document.body.addEventListener('click', closeDropdown)
+
+    return () => document.body.removeEventListener('click', closeDropdown)
+  }, [])
   return (
-    <div className="relative flex items-center">
+    <div className="relative flex items-center" ref={menuRef}>
       <button
         className="hover:opacity-70 cursor-pointer"
         onClick={() => setOpen(!open)}
       >
         <RiMessage2Fill className="text-lg" />
       </button>
-      {open && <Dropdown title="Messages" />}
+      {open && (
+        <Dropdown title="Messages">
+          {notifications
+            .filter((n) => n.type === 'new_message')
+            .slice(0, 7)
+            .map((n) => {
+              return (
+                <div
+                  className="flex items-center gap-5 p-1 border-b border-gray-600"
+                  key={n.id}
+                >
+                  <MdOutlineMessage className="text-2xl" />
+                  <p className="text-sm text-gray-400">{n.content}</p>
+                </div>
+              )
+            })}
+        </Dropdown>
+      )}
     </div>
   )
 }
