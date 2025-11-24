bee8b25ac516ed381360c219a3a444a9bee5be94
Oskar Fultowicz <oskarfultowicz@gmail.com>
2025-11-24 15:51:55 +0100

    fix prisma schema

--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -60,6 +60,7 @@ export const deleteUser: RequestHandler = async (req, res) => {
     res.status(200).json({ message: 'User deleted successfully' })
   } catch (error) {
     res.status(500).json({ message: 'There was an error' })
+    console.log(error)
   }
 }
 
